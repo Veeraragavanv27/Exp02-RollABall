06555b234e9f5c5168731067975f9ca3fca7852a
franklinraj <rajfranklin764@gmail.com>
2025-03-12 20:44:35 +0530

    initial commit

--- Assets/Scenes/Script/PlayerController.cs
+++ Assets/Scenes/Script/PlayerController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class move : MonoBehaviour
 {
-   
     // Start is called before the first frame update
+    public float xForce=5.0f;
+    public float zForce=5.0f;
+    public float yForce=100.0f;
+
     void Start()
     {
         
@@ -14,6 +17,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // type the code here
+          float x = 0.0f, y = 0.0f, z = 0.0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+           x = x - xForce; 
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+           x = x + xForce; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           z = z + zForce; 
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+           z = z - zForce; 
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           y = yForce;
+        }
+        
+        
+        GetComponent<Rigidbody>().AddForce(x,y,z);
+    
+        
     }
 }
